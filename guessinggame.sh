8827d385461c973189bb97f6b3775edfb7312b13
Odonnell30 <DannyLODonnell@hotmail.com>
2020-03-09 14:26:33 +0000

    Three Files updated

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,46 +1,24 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-# Global var to determine if another question round is needed
-bullseye=0
+count=0
 
-function get_files {
-	# Get the number of files only in the current directory. Not recurrant.
-	local lines=$(find . -maxdepth 1 -type f | wc -l | egrep -o "[0-9]+")
-	echo $lines
-}
+files=$(ls | wc -l | egrep -o "([0-9]+)")
 
-function compare_guess {
-	local files=$(get_files)
-	
-	if [[ $guess -eq $files ]]
-	then
-		echo "Congratulations! You got it right"
-		# set the global var to 1 to exit the while loop
-		bullseye=1
-	elif [[ $guess -lt $files ]]
-	then
-		echo "Not quite there yet. Your guess was too low. PLease try again."
-	else
-		echo "Almost... Your guess was too high. Please try again."
-	fi
-}
-
-echo "Please guess how many files are in the current directory:"
-
-while [[ $bullseye -eq 0 ]]
-do
+function user_guess {
+	echo "How many files do you think this directory contains?"
 	read guess
-	if [[ $(echo $guess | egrep -o "[^0-9]+") ]]
+}
+
+while [[ $guess -ne $files ]]
+do
+	user_guess
+	if [[ $guess -gt $files ]]
 	then
-		echo "Please type in a number:"
-		read guess
+		echo "Bad luck, too high. try again"
+	else [[ $guess -lt $files ]]
+		echo "Bad luck, too low. Try again"
+	user_guess
 	fi
-	
-	# Run the compare
-	compare_guess
 done
-
-
-
-
+echo "Well done! You guessed Right!"
